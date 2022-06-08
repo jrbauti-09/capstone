9762947894bd331117f6a50d08991767e0922aab
joshua <jrbauti1@asu.edu>
2022-06-08 09:43:14 -0700

    routes and forms partially done

--- app/forms/__init__.py
+++ app/forms/__init__.py
@@ -1,3 +1,5 @@
 from .login_form import LoginForm
 from .signup_form import SignUpForm
 from .thought_form import ThoughtForm
+from .review_form import ReviewForm
+from .ingredient_form import IngredientForm
