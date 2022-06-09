c6dbb2b66e5ca1cfba8e385cdb1266c470068acc
joshua <jrbauti1@asu.edu>
2022-06-09 11:03:03 -0700

    added useHistory to logoutButton

--- react-app/src/components/auth/LogoutButton.js
+++ react-app/src/components/auth/LogoutButton.js
@@ -1,9 +1,11 @@
-import React from 'react';
-import { useDispatch } from 'react-redux';
-import { logout } from '../../store/session';
+import React from "react";
+import { useDispatch } from "react-redux";
+import { logout } from "../../store/session";
+import { useHistory } from "react-router-dom";
 
 const LogoutButton = () => {
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
+  const history = useHistory();
   const onLogout = async (e) => {
     await dispatch(logout());
   };
