9fa2ad0c7947a31375cf94f645cff835045da4d1
joshua <jrbauti1@asu.edu>
2022-06-08 22:09:32 -0700

    modified review_route

--- app/api/review_routes.py
+++ app/api/review_routes.py
@@ -7,17 +7,33 @@ from app.models import db, Review
 
 review_routes = Blueprint('reviews', __name__)
 
+def validation_errors_to_error_messages(validation_errors):
+    """
+    Simple function that turns the WTForms validation errors into a simple list
+    """
+    errorMessages = []
+    for field in validation_errors:
+        for error in validation_errors[field]:
+            errorMessages.append(error)
+    return errorMessages
+
 # GET all reviews for thought
-@review_routes.route('/<int:recipeId>')
-def get(recipeId):
-    reviews = Review.query.filter(Review.thought_id == recipeId)
+# @review_routes.route('/<int:recipeId>')
+# def get(recipeId):
+#     reviews = Review.query.filter(Review.thought_id == recipeId)
+#     return {"reviews": [review.to_dict() for review in reviews]}
+
+@review_routes.route('/')
+def get_all():
+    reviews = Review.query.all()
     return {"reviews": [review.to_dict() for review in reviews]}
 
 # DELETE
-@review_routes.route('/<int:recipeId>')
+@review_routes.route('/<int:reviewId>', methods=["DELETE"])
 @login_required
-def delete_review(recipeId):
-    deleteReview = Review.query.filter(Review.id == recipeId).first()
+def delete_review(reviewId):
+    deleteReview = Review.query.filter(Review.id == reviewId).first()
+    print("LOOK AT ME HERE")
 
     if deleteReview:
         db.session.delete(deleteReview)
@@ -25,11 +41,12 @@ def delete_review(recipeId):
         return {"id": deleteReview.id}
 
 # POST
-@review_routes.route('/')
+@review_routes.route('/', methods=['POST'])
 @login_required
 def post_review():
     form = ReviewForm()
     form["csrf_token"].data = request.cookies["csrf_token"]
+    print(form.validate_on_submit(), 'what is happening')
     if form.validate_on_submit():
         data = form.data
         review = Review(
@@ -41,7 +58,7 @@ def post_review():
         db.session.add(review)
         db.session.commit()
         return review.to_dict()
-    return {"errors": validation_errors_to_error_messages(form.errors)}
+    return {"errors": validation_errors_to_error_messages(form.errors)}, 401
 
 # PUT
 @review_routes.route('/<int:reviewId>', methods=['PUT'])
