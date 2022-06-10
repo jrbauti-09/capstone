d1aaa5568dcc6d3259a34e93e4ea976e7666e0db
joshua <jrbauti1@asu.edu>
2022-06-09 19:13:00 -0700

    backend validators on forms

--- app/forms/thought_form.py
+++ app/forms/thought_form.py
@@ -3,8 +3,14 @@ from wtforms import StringField, SelectField, IntegerField, SubmitField, TextAre
 from wtforms.validators import DataRequired, Email, ValidationError
 from app.models import Thought
 
+
+def name_check(form, field):
+    name = field.data
+    if len(name) > 255:
+        raise ValidationError("Name length cannot exceed 255 characters.")
+
 class ThoughtForm(FlaskForm):
-    name = StringField('Name', validators=[DataRequired()])
+    name = StringField('Name', validators=[DataRequired(), name_check])
     description = StringField('Description', validators=[DataRequired()])
     instructions = TextAreaField('Instruction', validators=[DataRequired()])
     user_id = IntegerField("UserId", validators=[DataRequired()])
