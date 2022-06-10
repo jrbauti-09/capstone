529b5e4e377d4c052b2bbd5beeff299e753eb035
joshua <jrbauti1@asu.edu>
2022-06-09 19:34:43 -0700

    finished thunk for ingredients

--- react-app/src/store/ingredient.js
+++ react-app/src/store/ingredient.js
@@ -36,5 +36,94 @@ const ingredientToDelete = (deletedIngredient) => {
 // Thunk
 
 export const getIngredients = () => async (dispatch) => {
-  const response = await fetch("/api/ingredients");
+  const response = await fetch("/api/ingredients/");
+
+  if (response.ok) {
+    const ingredients = await response.json();
+    dispatch(load(ingredients.ingredients));
+  }
+};
+
+export const addIngredient = (data) => async (dispatch) => {
+  const response = await fetch("/api/ingredients/", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(data),
+  });
+
+  if (response.ok) {
+    const ingredient = await response.json();
+    dispatch(add(ingredient));
+  } else {
+    const data = await response.json();
+    if (data.errors) {
+      return { errors: data.errors };
+    }
+  }
+};
+
+export const editIngredient = (data, ingredient_id) => async (dispatch) => {
+  const response = await fetch(`/api/ingredients/${ingredient_id}`, {
+    method: "PUT",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(data),
+  });
+
+  if (response.ok) {
+    const editedIngredient = await response.json();
+    dispatch(edit(editedIngredient));
+  } else {
+    const data = await response.json();
+    if (data.errors) {
+      return { errors: data.errors };
+    }
+  }
+};
+
+export const deleteIngredient = (ingredient_id) => async (dispatch) => {
+  const response = await fetch(`/api/ingredients/${ingredient_id}`, {
+    method: "DELETE",
+  });
+
+  if (response.ok) {
+    const data = await response.json();
+    dispatch(ingredientToDelete(data));
+  }
+};
+
+const initialState = {};
+
+const ingredientReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case LOAD_INGREDIENTS:
+      const allIngredients = {};
+      action.ingredients.forEach((ingredient) => {
+        allIngredients[ingredient.id] = ingredient;
+      });
+
+      return {
+        ...state,
+        ...allIngredients,
+      };
+    case ADD_INGREDIENTS:
+      const newState = {
+        ...state,
+        [action.ingredient.id]: action.ingredient,
+      };
+      return newState;
+    case EDIT_INGREDIENTS:
+      const editState = {
+        ...state,
+        [action.ingredient.id]: action.ingredient,
+      };
+      return editState;
+    case DELETE_INGREDIENTS:
+      const state = {
+        ...state,
+      };
+      delete state[action.ingredient.id];
+      return state;
+    default:
+      return state;
+  }
 };
